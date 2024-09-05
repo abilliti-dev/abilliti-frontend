5ca0d4d7d66e08e690eb99680ff4b9ce8a431b07
mva919 <mva919@gmail.com>
2024-09-05 13:51:36 -0700

    Remove camal case named files

--- src/screens/sign-up/components/SignUpCompleteSlide.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function SignUpCompleteSlide() {
-  return (
-    <div className="w-full flex items-center h-full flex-col gap-4">
-      <div className="bg-white w-[55%] min-w-[325px] shadow rounded-2xl px-6 py-10"></div>
-    </div>
-  );
-}
--- src/screens/sign-up/components/VerifyAccountSlide.tsx
+++
@@ -1,7 +0,0 @@
-export default function VerifyAccountSlide() {
-  return (
-    <div className="w-full flex items-center h-full flex-col gap-4">
-      <div className="bg-white w-[55%] min-w-[325px] shadow rounded-2xl px-6 py-10"></div>
-    </div>
-  );
-}
