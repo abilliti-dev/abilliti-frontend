3c139d73366c0aac6fa5578d4198b471a662d595
Emanuel Pecson <emanpecson@gmail.com>
2024-10-07 14:49:02 -0700

    modify icon-button to prevent button's classname from overwrite

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -9,9 +9,11 @@ interface IconButtonProps extends ButtonProps {
 
 export default function IconButton(props: IconButtonProps) {
   return (
-    <Button className="flex space-x-1.5 place-items-center" {...props}>
-      <props.Icon size={props.iconSize ?? 20} strokeWidth={props.iconStrokeWidth ?? 1.5} />
-      <div>{props.children}</div>
+    <Button {...props}>
+      <div className="flex space-x-1.5 place-items-center">
+        <props.Icon size={props.iconSize ?? 20} strokeWidth={props.iconStrokeWidth ?? 1.5} />
+        <div>{props.children}</div>
+      </div>
     </Button>
   );
 }
