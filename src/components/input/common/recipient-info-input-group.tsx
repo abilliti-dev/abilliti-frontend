a567604a45c37dc9198c8284d0a8020188d8c179
Emanuel Pecson <emanpecson@gmail.com>
2024-10-07 18:45:00 -0700

    add recipient-info-input-group

---
+++ src/components/input/common/recipient-info-input-group.tsx
@@ -0,0 +1,33 @@
+import { MailIcon, PhoneIcon, UserIcon } from "lucide-react";
+import InputGroup from "../input-group";
+import TextInput from "../text-input";
+
+export default function RecipientInfoInputGroup() {
+  return (
+    <InputGroup
+      row1Inputs={[
+        (props) => (
+          <TextInput
+            {...props}
+            label="Client name"
+            placeholder="Enter a client name"
+            Icon={UserIcon}
+          />
+        ),
+      ]}
+      row2Inputs={[
+        (props) => (
+          <TextInput {...props} label="Email" placeholder="janedoe@gmail.com" Icon={MailIcon} />
+        ),
+        (props) => (
+          <TextInput
+            {...props}
+            label="Phone number"
+            placeholder="(000) 000-0000"
+            Icon={PhoneIcon}
+          />
+        ),
+      ]}
+    />
+  );
+}
