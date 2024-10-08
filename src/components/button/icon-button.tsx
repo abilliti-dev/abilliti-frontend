df83528467c29abed1c183118d7ae753aba52b71
Emanuel Pecson <emanpecson@gmail.com>
2024-10-08 00:37:20 -0700

    update button styling

--- src/components/button/icon-button.tsx
+++ src/components/button/icon-button.tsx
@@ -1,17 +1,23 @@
 import { LucideIcon } from "lucide-react";
 import { Button, ButtonProps } from "../ui/button";
+import { ClassNameValue } from "tailwind-merge";
 
 interface IconButtonProps extends ButtonProps {
   Icon: LucideIcon;
   iconSize?: number;
   iconStrokeWidth?: number;
+  iconClassName?: ClassNameValue;
 }
 
 export default function IconButton(props: IconButtonProps) {
   return (
     <Button {...props}>
       <div className="flex space-x-1.5 place-items-center">
-        <props.Icon size={props.iconSize ?? 20} strokeWidth={props.iconStrokeWidth ?? 1.5} />
+        <props.Icon
+          size={props.iconSize ?? 20}
+          strokeWidth={props.iconStrokeWidth ?? 1.5}
+          className={props.iconClassName as string}
+        />
         <div>{props.children}</div>
       </div>
     </Button>
