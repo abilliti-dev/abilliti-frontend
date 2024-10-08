337e01c607a3a888b71beb42352cbaf0193b1208
Emanuel Pecson <emanpecson@gmail.com>
2024-10-07 17:51:05 -0700

    add more rounding options in input-group

--- src/components/input/input-group.tsx
+++ src/components/input/input-group.tsx
@@ -2,28 +2,49 @@ import { BaseInputProps } from "./base-input";
 
 interface InputGroupProps {
   row1Inputs: ((props: BaseInputProps) => JSX.Element)[];
-  row2Inputs: ((props: BaseInputProps) => JSX.Element)[];
+  row2Inputs?: ((props: BaseInputProps) => JSX.Element)[];
 }
 
 export default function InputGroup(props: InputGroupProps) {
   return (
-    <div className="grid grid-rows-2 divide-y rounded-xl border">
+    <div className="grid grid-flow-row divide-y rounded-xl border">
       <div className="grid grid-flow-col divide-x">
         {props.row1Inputs.map((Input, i) => {
+          const isSingleRow = props.row2Inputs === undefined || props.row2Inputs.length === 0;
+          const isAlone = props.row1Inputs.length === 1;
           const isFirst = i === 0;
           const isLast = i === props.row1Inputs.length - 1;
-          const rounding = isFirst ? "rounded-tl-xl" : isLast ? "rounded-tr-xl" : "rounded-none";
+          const rounding = isAlone
+            ? "rounded-t-xl"
+            : isSingleRow && isFirst
+            ? "rounded-l-xl"
+            : isSingleRow && isLast
+            ? "rounded-r-xl"
+            : isFirst
+            ? "rounded-tl-xl"
+            : isLast
+            ? "rounded-tr-xl"
+            : "rounded-none";
           return <Input rounding={rounding} hideBorder />;
         })}
       </div>
-      <div className="grid grid-flow-col divide-x">
-        {props.row2Inputs.map((Input, i) => {
-          const isFirst = i === 0;
-          const isLast = i === props.row2Inputs.length - 1;
-          const rounding = isFirst ? "rounded-bl-xl" : isLast ? "rounded-br-xl" : "rounded-none";
-          return <Input rounding={rounding} hideBorder />;
-        })}
-      </div>
+      {props.row2Inputs && props.row2Inputs.length > 0 && (
+        <div className="grid grid-flow-col divide-x">
+          {props.row2Inputs.map((Input, i) => {
+            const isAlone = props.row1Inputs.length === 1;
+            const isFirst = i === 0;
+            const isLast = props.row2Inputs && i === props.row2Inputs.length - 1;
+            const rounding = isAlone
+              ? "rounded-b-xl"
+              : isFirst
+              ? "rounded-bl-xl"
+              : isLast
+              ? "rounded-br-xl"
+              : "rounded-none";
+            return <Input rounding={rounding} hideBorder />;
+          })}
+        </div>
+      )}
     </div>
   );
 }
