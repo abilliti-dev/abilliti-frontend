d845a79776fcc3ad1a64a30016ec17a30be5f47b
Emanuel Pecson <emanpecson@gmail.com>
2024-10-10 20:52:30 -0700

    style: :lipstick: add dropdown-icon on date-input

--- src/components/input/date-input.tsx
+++ src/components/input/date-input.tsx
@@ -1,37 +1,41 @@
 import { cn } from "@/lib/utils";
-// import { Input } from "../ui/input";
 import BaseInput, { BaseInputProps } from "./base-input";
 import { format } from "date-fns";
-import { CalendarIcon } from "lucide-react";
-import { Button } from "../ui/button";
+import { CalendarDaysIcon, ChevronDownIcon } from "lucide-react";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { useState } from "react";
 import { Calendar } from "@/components/ui/calendar";
+import { Button } from "../ui/button";
 
 export default function DateInput(props: BaseInputProps) {
   const [date, setDate] = useState<Date>();
 
   return (
-    <BaseInput label={props.label ?? "Date"} Icon={CalendarIcon}>
+    <BaseInput label={props.label ?? "Date"} Icon={CalendarDaysIcon}>
       <Popover>
         <PopoverTrigger asChild>
           <Button
-            variant={"outline"}
             className={cn(
-              "pl-12",
               props.hideBorder && "border-none",
               props.rounding ?? "rounded-xl",
-              "h-12 pt-6 focus-visible:ring-[2.5px] focus-visible:ring-green-secondary focus-visible:ring-offset-0 focus-visible:ring-inset placeholder:text-neutral-400 text-xs w-full"
+              "pl-12 h-12 text-xs w-full relative hover:bg-opacity-0"
             )}
+            variant={"ghost"}
           >
             <text
               className={cn(
                 date ? "text-neutral-800" : "text-neutral-400",
-                "absolute left-12 text-xs"
+                "absolute left-12 bottom-1.5 text-xs"
               )}
             >
               {date ? format(date, "PPP") : props.placeholder ?? "Select a date"}
             </text>
+
+            <ChevronDownIcon
+              className="absolute right-3 bottom-3 text-neutral-500 pointer-events-none"
+              strokeWidth={1.5}
+              size={20}
+            />
           </Button>
         </PopoverTrigger>
         <PopoverContent className="w-auto p-0">
