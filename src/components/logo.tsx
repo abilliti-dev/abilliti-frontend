dc52e8071c1e9a5e8eaad578fbd27cc04392a5e8
Marcos Villanueva <mva919@gmail.com>
2024-10-28 11:18:53 -0700

    Fix small abilliti logo not being fetched

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -8,11 +8,7 @@ export interface LogoProps {
 export default function Logo(props: LogoProps) {
   return (
     <img
-      src={
-        props.variant && props.variant == "sm"
-          ? "/src/assets/abilliti-logo-small.svg"
-          : "/Abilliti.svg"
-      }
+      src={props.variant && props.variant == "sm" ? "/abilliti-logo-small.svg" : "/Abilliti.svg"}
       alt="logo"
       className={cn(props.className, "cursor-pointer max-h-[350px]")}
       onClick={() => {
