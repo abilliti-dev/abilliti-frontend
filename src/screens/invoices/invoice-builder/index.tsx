496788fd8f59e18d39d2913e91926ef458cbf405
Emanuel Pecson <emanpecson@gmail.com>
2024-11-03 18:08:51 -0800

    integrate dashboard layout into invoice-builder

--- src/screens/invoices/invoice-builder/index.tsx
+++ src/screens/invoices/invoice-builder/index.tsx
@@ -3,27 +3,37 @@ import InvoicePreview from "./components/invoice-preview";
 import SectionContainer from "./components/section/section-container";
 import Stepper from "./components/stepper";
 import { sections } from "./data/sections";
+import DashboardLayout from "@/layouts/dashboard-layout";
+import DashboardContextProvider from "@/contexts/dashboard-context";
 
 export default function InvoiceBuilderPage() {
   const [step, setStep] = useState<number>(1);
 
   return (
-    <div className="grid grid-cols-2 h-screen">
-      <div className="col-span-1 p-10 flex justify-end place-items-start">
-        <InvoicePreview />
-      </div>
-      <div className="col-span-1 p-10 flex justify-start place-items-start">
-        <div className="w-full">
-          <Stepper
-            labels={Object.values(sections).map((x) => x.label)}
-            step={step}
-            setStep={setStep}
-          />
-          <SectionContainer setStep={setStep} step={step} stepAmount={Object.keys(sections).length}>
-            {sections[step as keyof typeof sections].section}
-          </SectionContainer>
+    <DashboardContextProvider>
+      <DashboardLayout>
+        <div className="grid grid-cols-2">
+          <div className="col-span-1 p-10 flex justify-end place-items-start">
+            <InvoicePreview />
+          </div>
+          <div className="col-span-1 p-10 flex justify-start place-items-start">
+            <div className="w-full">
+              <Stepper
+                labels={Object.values(sections).map((x) => x.label)}
+                step={step}
+                setStep={setStep}
+              />
+              <SectionContainer
+                setStep={setStep}
+                step={step}
+                stepAmount={Object.keys(sections).length}
+              >
+                {sections[step as keyof typeof sections].section}
+              </SectionContainer>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </DashboardLayout>
+    </DashboardContextProvider>
   );
 }
