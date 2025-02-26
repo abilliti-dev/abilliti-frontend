5a325ed0d166f9f386787bdb9a58fa782bf31af1
Emanuel Pecson <emanpecson@gmail.com>
2025-02-25 19:00:52 -0800

    smaller invoice-preview

--- src/screens/invoices/invoice-builder/components/invoice-preview.tsx
+++ src/screens/invoices/invoice-builder/components/invoice-preview.tsx
@@ -1,6 +1,6 @@
 export default function InvoicePreview() {
   return (
-    <div className="border shadow-lg bg-white w-[min(35vw,500px)] aspect-[8.5/11] shrink-0 h-fit" />
+    <div className="border shadow-lg bg-white w-[min(30vw,500px)] aspect-[8.5/11] shrink-0 h-fit" />
   );
 
   // aspect-[8.5/11] -> letter page shape
