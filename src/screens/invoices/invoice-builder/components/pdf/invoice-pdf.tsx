a880bc0f4e5eea66a07f59c284b3a524fa0e376b
Emanuel Pecson <emanpecson@gmail.com>
2025-04-08 22:13:18 -0700

    add components for invoice pdf layout

---
+++ src/screens/invoices/invoice-builder/components/pdf/invoice-pdf.tsx
@@ -0,0 +1,150 @@
+import { InvoiceForm } from "@/types/invoice-form";
+import { Document, Page, PDFViewer, View, Text, Image } from "@react-pdf/renderer";
+import ItemsTable from "./table/items-table";
+import ItemsTableData from "./table/items-table-data";
+import { styles } from "./styles";
+import {
+  calculateSubtotal,
+  calculateTax,
+  calculateTotal,
+  taxRateStringToNumber,
+} from "../../util/calculate";
+import formatMoney from "../../util/format-money";
+
+interface InvoicePDFProps {
+  invoice: InvoiceForm;
+}
+
+export default function InvoicePDF(props: InvoicePDFProps) {
+  const { general, company, client, itemsAndCosts } = props.invoice;
+  const subtotal = calculateSubtotal(itemsAndCosts.items);
+  const total = calculateTotal(itemsAndCosts.taxRate, itemsAndCosts.discount, subtotal);
+  const taxRate = taxRateStringToNumber(props.invoice.itemsAndCosts.taxRate);
+  const tax = calculateTax(subtotal, taxRate);
+
+  const PDF = () => (
+    <Document>
+      <Page size="A4" style={styles.ySpaceLarge}>
+        <View style={[styles.invoiceDetailsContainer, styles.ySpaceLarge]}>
+          <View style={[styles.flexRow]}>
+            <View style={styles.ySpaceMedium}>
+              <View>
+                <Text style={styles.title}>INVOICE</Text>
+                <Text>Invoice ID (Generated)</Text>
+              </View>
+
+              <View style={styles.ySpaceSmall}>
+                <Text style={styles.h1}>{company.name || "Company name"}</Text>
+
+                <View>
+                  <Text>{company.address.street || "Street"}</Text>
+                  <Text>
+                    {company.address.city || "City"}, {company.address.state || "State"}{" "}
+                    {company.address.zipCode || "Zip code"}
+                  </Text>
+                  <Text>{company.phone || "Phone number"}</Text>
+                  <Text>{company.email || "Email address"}</Text>
+                </View>
+              </View>
+            </View>
+
+            <Image src={props.invoice.company.image} style={styles.logo} />
+          </View>
+
+          <View style={styles.flexRow}>
+            <View style={styles.ySpaceMedium}>
+              <Text style={styles.h2}>BILLED TO</Text>
+              <View>
+                <Text>{client.name || "Client name"}</Text>
+                <Text>
+                  {client.address.city || "City"}, {client.address.state || "State"}{" "}
+                  {client.address.zipCode || "Zip code"}
+                </Text>
+                <Text>{client.phone || "Phone number"}</Text>
+                <Text>{client.email || "Email address"}</Text>
+              </View>
+            </View>
+
+            <View style={styles.ySpaceMedium}>
+              <Text style={styles.h2}>JOB DETAILS</Text>
+              <Text>{general.description || "Job description"}</Text>
+            </View>
+
+            <View style={styles.ySpaceMedium}>
+              <View style={styles.ySpaceMedium}>
+                <Text style={styles.h2}>INVOICE DATE</Text>
+                <Text>{new Date(general.date.issue).toLocaleDateString()}</Text>
+              </View>
+
+              <View style={styles.ySpaceMedium}>
+                <Text style={styles.h2}>PAYMENT DUE</Text>
+                <Text>
+                  {general.date.due ? new Date(general.date.due).toLocaleDateString() : "N/A"}
+                </Text>
+              </View>
+            </View>
+          </View>
+        </View>
+
+        <View style={[styles.itemsAndCostsContainer, styles.ySpaceMedium]}>
+          <ItemsTable>
+            {itemsAndCosts.items.map((item, i) => (
+              <ItemsTableData item={item} key={i} />
+            ))}
+          </ItemsTable>
+
+          <View style={styles.flexRow}>
+            <View>
+              <Text style={styles.fontSemibold}>Notes</Text>
+              <Text>{itemsAndCosts.notes || "Enter notes, comments, or terms/conditions"}</Text>
+            </View>
+
+            <View style={[styles.ySpaceMedium, styles.totalsContainer]}>
+              <View>
+                <View style={styles.flexRow}>
+                  <Text>Subtotal</Text>
+                  <Text>{formatMoney(subtotal)}</Text>
+                </View>
+
+                <View style={styles.flexRow}>
+                  <Text>Tax rate</Text>
+                  <Text>
+                    {itemsAndCosts.taxRate && itemsAndCosts.taxRate !== "0"
+                      ? itemsAndCosts.taxRate
+                      : "0.0%"}
+                  </Text>
+                </View>
+
+                <View style={styles.flexRow}>
+                  <Text>Tax</Text>
+                  <Text>+ {formatMoney(tax)}</Text>
+                </View>
+
+                <View style={styles.flexRow}>
+                  <Text>Discount</Text>
+                  <Text>
+                    -{" "}
+                    {itemsAndCosts.discount && itemsAndCosts.discount !== "0"
+                      ? itemsAndCosts.discount
+                      : "$0.00"}
+                  </Text>
+                </View>
+              </View>
+
+              <View style={[styles.flexRow, styles.fontSemibold]}>
+                <Text>Total</Text>
+                <Text>{formatMoney(total)}</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </Page>
+    </Document>
+  );
+
+  return (
+    <PDFViewer width="100%" height={700}>
+      <PDF />
+    </PDFViewer>
+  );
+}
---
+++ src/screens/invoices/invoice-builder/components/pdf/styles.tsx
@@ -0,0 +1,86 @@
+import { StyleSheet } from "@react-pdf/renderer";
+
+export const styles = StyleSheet.create({
+  tableContainer: {
+    rowGap: 6,
+  },
+
+  tableRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  tableColumnWidthPrimary: {
+    width: "290px",
+    textAlign: "left",
+  },
+
+  tableColumnWidthSecondary: {
+    width: "60px",
+    textAlign: "right",
+  },
+
+  tableDivider: {
+    height: 1,
+    width: 495,
+    backgroundColor: "#262626",
+  },
+
+  fontSemibold: {
+    fontWeight: 600,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: 600,
+    color: "#262626",
+  },
+
+  invoiceDetailsContainer: {
+    fontSize: "12px",
+    padding: "30px 50px",
+  },
+
+  itemsAndCostsContainer: {
+    backgroundColor: "#e5e5e5",
+    fontSize: "12px",
+    padding: "20px 50px",
+  },
+
+  ySpaceSmall: {
+    rowGap: 8,
+  },
+
+  ySpaceMedium: {
+    rowGap: 16,
+  },
+
+  ySpaceLarge: {
+    rowGap: 24,
+  },
+
+  h1: {
+    fontWeight: 500,
+    fontSize: 14,
+  },
+
+  h2: {
+    color: "#8E8E8E",
+    fontSize: 9,
+    fontWeight: 600,
+  },
+
+  totalsContainer: {
+    width: 180,
+  },
+
+  flexRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  logo: {
+    width: 64,
+    height: 64,
+  },
+});
