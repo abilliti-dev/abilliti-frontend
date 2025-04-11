35e0923ad331e2ba2b081add18c970f7e1a95116
Emanuel Pecson <emanpecson@gmail.com>
2025-04-10 23:13:04 -0700

    allow due date to be same-day

--- src/types/schema/general-invoice-info-schema.ts
+++ src/types/schema/general-invoice-info-schema.ts
@@ -9,8 +9,8 @@ export const generalInvoiceInfoSchema = z.object({
       issue: z.date({ message: "Required" }),
       due: z.date({ message: "Required" }),
     })
-    .refine((data) => data.issue <= data.due, {
-      message: "Due date must be later than the issue date",
+    .refine((data) => data.issue < data.due, {
+      message: "Due date must be on or after the issue date",
       path: ["due"], // "point error to "due"
     }),
 });
