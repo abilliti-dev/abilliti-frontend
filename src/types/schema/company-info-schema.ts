c27ace4edbb39c6c888bbcf8a36cf5b87645564c
Emanuel Pecson <emanpecson@gmail.com>
2025-04-14 19:56:35 -0700

    reduce max char count on client/company info

--- src/types/schema/company-info-schema.ts
+++ src/types/schema/company-info-schema.ts
@@ -7,9 +7,9 @@ export type CompanyInfoFormFields = z.infer<typeof companyInfoSchema>;
 const prefixExceeded = "Exceeded max character length of";
 
 export const companyInfoSchema = z.object({
-  name: z.string().min(1, "Required").max(60, `${prefixExceeded} 60`),
+  name: z.string().min(1, "Required").max(40, `${prefixExceeded} 40`),
   image: imageSchema,
-  email: z.string().min(1, "Required").email().max(60, `${prefixExceeded} 60`),
+  email: z.string().min(1, "Required").email().max(40, `${prefixExceeded} 40`),
   phone: z
     .string()
     .min(1)
